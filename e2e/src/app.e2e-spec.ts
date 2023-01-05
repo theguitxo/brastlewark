af3f09eed5832a5533d5d84ffce051e8ca9128ce
Amador <guitxo@guitxo.com>
2023-01-05 13:30:05 +0100

    actualizado titulo aplicaciòn

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('brastlewarkA9 app is running!');
+    expect(page.getTitleText()).toEqual('brastlewark app is running!');
   });
 
   afterEach(async () => {
